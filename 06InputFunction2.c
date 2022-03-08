832214a114cacd89ee7569fe28f6779184a0d638
RohanSanjayPol <polrohan90@gmail.com>
2022-03-08 11:12:00 +0000

    c1

---
+++ 06InputFunction2.c
@@ -0,0 +1,11 @@
+// input function
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a;
+    float b;
+    printf("Enter a number: ");
+    scanf("%d" "%f",&a,&b);
+    printf("%d\n" "%f\n",a,b);
+   
+}
