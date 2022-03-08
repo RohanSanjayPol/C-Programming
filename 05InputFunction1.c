eda54b8d9d6d603fa295d7ee5066fab1b59e4423
RohanSanjayPol <polrohan90@gmail.com>
2022-03-08 10:30:00 +0000

    c1

---
+++ 05InputFunction1.c
@@ -0,0 +1,31 @@
+// formatted input function
+#include<stdio.h>
+#include<conio.h>
+void main(){
+// addition of two numbers by taking input from user
+int no1,no2,add;
+printf("Enter no 1: ");
+scanf("%d",&no1);
+printf("Enter a no 2: ");
+scanf("%d",&no2);
+ 
+add=no1+no2;
+printf("%d\n",add); 
+
+//second task
+// take character value
+
+char name;
+printf("Enter letter: ");
+scanf(" %c",&name);
+printf("%c\n",name);
+
+
+//Enter a string
+char fName;
+printf("Enter your name");
+scanf(" %c\n",&fName);
+printf(fName);
+}
+
+
