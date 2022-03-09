77db73bf937ebb71aff27869cbb3471862ccc11a
RohanSanjayPol <polrohan90@gmail.com>
2022-03-09 17:15:00 +0000

    c

---
+++ 13Array.c
@@ -0,0 +1,23 @@
+// run time array initialization
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a[5];
+    int i;
+
+    printf("Enter 5 numbers: ");
+    
+    for(i=0; i<5; i++){
+        scanf("%d", &a[i]);
+    }
+    
+    printf("Output is: ");
+    
+    for(i=0; i<5; i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+    getch();
+}
+
