facb631bb9a090e4ec75ea0bd9b0dc3d764f8ddb
RohanSanjayPol <polrohan90@gmail.com>
2022-03-09 15:43:00 +0000

    c

---
+++ 08OutputFunction.c
@@ -0,0 +1,13 @@
+// addition of two numbers 
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,sum;
+    printf("Enter a first numbers: ");
+    scanf("%d",&a);
+    printf("Enter a second number: ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("the sum of two number %d and %d is %d",a,b,sum);
+}
