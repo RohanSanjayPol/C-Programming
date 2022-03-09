838bacea6d28a40ab06f02a41720587ba878b563
RohanSanjayPol <polrohan90@gmail.com>
2022-03-09 15:55:00 +0000

    c

---
+++ 09UnformattedInputFunction.c
@@ -0,0 +1,11 @@
+// unformatted input function
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    char alphabet;
+    printf("Enter a character: ");
+    alphabet=getchar();
+    printf(" character is %c\n",alphabet);
+}
+
