7d0dc244dcb3ba53b078ad118e50705f3da38322
RohanSanjayPol <polrohan90@gmail.com>
2022-03-12 04:09:00 +0000

    c

---
+++ 21Array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    //2d array
+    int storage1,storage2,i;
+    printf("Enter a row size of an array");
+    int row_size=scanf("%d",&storage1);
+    printf("Enter a column size of an array");
+    int col_size=scanf("%d",&storage2);
+    int arr[row_size][col_size];
+    for(i=0;i<row_size;i++)
+}
