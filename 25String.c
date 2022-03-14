f105b625c90dae3cd19a7e8ba301c90311371e85
RohanSanjayPol <polrohan90@gmail.com>
2022-03-14 17:07:00 +0000

    c

---
+++ 25String.c
@@ -0,0 +1,14 @@
+// taking string input from user by using scanf and gets function
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    //declaring string of size 10
+    char name[10],fName[10];
+    printf("Enter your name:\n ");
+    //taking string input using scanf function
+    scanf("%s ",name);
+    printf("your name is : %s\n",name);
+
+  
+}
