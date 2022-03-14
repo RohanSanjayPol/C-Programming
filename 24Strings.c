b74ae595ac641f37596bfdf2f9f2f9aeacb18d89
RohanSanjayPol <polrohan90@gmail.com>
2022-03-14 16:46:00 +0000

    c

---
+++ 24Strings.c
@@ -0,0 +1,14 @@
+//strings
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    //string declaration
+char str[10]={"this is rohan pol"};
+printf("%s\n",str);
+
+char str1[10]={'r','o','h','a','n'};
+printf("%s\n",str1);
+
+char str3[]={"this is ana de armas"};
+printf("%s",str3);
+}
