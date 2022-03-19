e7d09cd8da456b08355b6a659d3789707801c743
RohanSanjayPol <polrohan90@gmail.com>
2022-03-19 12:06:00 +0000

    c

---
+++ 29StringConcatination.c
@@ -0,0 +1,11 @@
+// string concatination using strcat function
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main(){
+char str1[30]="Rohan";
+char str2[5]="pol";
+strcat(str1,str2);
+puts(str1);
+}
