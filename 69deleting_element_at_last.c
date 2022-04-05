848ca9ffca9ede447fc9a625d881f524e88ebe3f
RohanSanjayPol <polrohan90@gmail.com>
2022-04-05 17:08:00 +0000

    c

---
+++ 69deleting_element_at_last.c
@@ -0,0 +1,57 @@
+// delete element at beginning from linked list
+
+#include<stdio.h>
+#include<conio.h>
+struct node{
+int data;
+struct node *next;
+};
+void main(){
+// declaring pointer with struct node datatype
+struct node *head,*newnode,*temp;
+head=0;
+
+//dynamic memory allocation using malloc function
+
+int i=0;
+printf("Enter the 5 Elements\n");
+while(i<5){
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("Enter Element: ",i);
+scanf("%d",&newnode->data);
+newnode->next=0;
+if(head==0){
+    head=temp=newnode;
+}else{
+    temp->next=newnode;
+    temp=newnode;
+}
+i++;
+}
+temp=head;
+while(temp!=0){
+printf(" %d -> ",temp->data);
+temp=temp->next;
+}
+printf("NULL\n");
+
+// deleting element at last
+temp=head;
+while(temp->next->next!=0){
+temp=temp->next;
+}
+free(temp->next->next);
+temp->next=0;
+
+
+//printing elements after deletion operation
+printf("deleting last Element \n");
+temp=head;
+while(temp!=0){
+printf(" %d -> ",temp->data);
+temp=temp->next;
+}
+printf("NULL\n");
+
+}
+
