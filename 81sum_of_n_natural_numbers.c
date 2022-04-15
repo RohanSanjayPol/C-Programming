885134dc7abd4a1cf91dc7d7012d7650a854cc0f
RohanSanjayPol <polrohan90@gmail.com>
2022-04-15 17:47:00 +0000

    c

---
+++ 81sum_of_n_natural_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int sum(int n){
+    int s=0;
+    if(n==0){
+        return n;
+    }
+    s=n+sum(n-1);
+    return s;
+    
+}
+void main(){
+    int total;
+   total= sum(5);
+   printf("%d",total);
+}
