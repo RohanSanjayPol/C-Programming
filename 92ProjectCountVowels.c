84e96022efa590c2aab7f423d8c6953b72a8f996
RohanSanjayPol <polrohan90@gmail.com>
2022-04-29 11:01:00 +0000

    c

---
+++ 92ProjectCountVowels.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[50];
+    int i,count=0;
+    printf("enter string: ");
+    gets(str);
+
+    for(i=0; i<strlen(str); i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+           str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+            count++;
+        }
+    }
+    printf("vowels = %d",count);
+    return 0;
+}
